d32f51be8c222d4121cb264b6045293fdcd973bf
Oleg991 <o.n.eremenko@gmail.com>
2023-02-04 10:35:06 +0300

    Initial Commit

---
+++ Shared SwiftUI Content/Shared_SwiftUI_ContentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shared_SwiftUI_ContentApp.swift
+//  Shared SwiftUI Content
+//
+//  Created by Олег Еременко on 04.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Shared_SwiftUI_ContentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
