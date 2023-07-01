af3b9bbd2c9e4d2dc89c609209fe54e214044a07
Oleg991 <o.n.eremenko@gmail.com>
2023-07-01 14:43:56 +0300

    добавив урок про fill vs foregroundColor

---
+++ Shared SwiftUI Content/FillVSForegroundColorExample.swift
@@ -0,0 +1,27 @@
+//
+//  FillVSForegroundColorExample.swift
+//  Shared SwiftUI Content
+//
+//  Created by Олег Еременко on 01.07.2023.
+//
+
+import SwiftUI
+
+struct FillVSForegroundColorExample: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Circle() // 1 - круг, залитый градиентом
+                .fill(Gradient(colors: [.blue, .black]))
+            Circle() // 2 - круг, залитый сплошным цветом
+                .fill(.blue)
+            Circle() // 3 - тоже круг, залитый сплошным цветом
+                .foregroundColor(.blue)
+        }
+    }
+}
+
+struct FillVSForegroundColorExample_Previews: PreviewProvider {
+    static var previews: some View {
+        FillVSForegroundColorExample()
+    }
+}
