10ee4aee13405f22e8c92baa9f8fd54e178941d2
Oleg991 <o.n.eremenko@gmail.com>
2023-09-03 12:22:20 +0300

    Добавил урок по printChanges

--- Shared SwiftUI Content/Shared_SwiftUI_ContentApp.swift
+++ Shared SwiftUI Content/Shared_SwiftUI_ContentApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Shared_SwiftUI_ContentApp: App {
     var body: some Scene {
         WindowGroup {
-            PluralsLocalizationExample()
+            PrintChangesExample()
         }
     }
 }
