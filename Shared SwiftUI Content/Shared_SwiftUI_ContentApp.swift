e01df542b59114634357f2f4540658c59a1f1439
Oleg991 <o.n.eremenko@gmail.com>
2025-03-10 09:38:03 +0300

    Добавил код для статьи № 111
    Снимок карты

--- Shared SwiftUI Content/Shared_SwiftUI_ContentApp.swift
+++ Shared SwiftUI Content/Shared_SwiftUI_ContentApp.swift
@@ -4,7 +4,17 @@ import SwiftUI
 struct Shared_SwiftUI_ContentApp: App {
     var body: some Scene {
         WindowGroup {
-            AddToCalendarExample()
+            let items: [MapSnapshotExample.MapModel] = [5000, 2500, 1250, 600, 300].map {
+                .init(latitude: 55.795396, longitude: 37.762597, locationDistance: $0)
+            }
+            return ScrollView {
+                LazyVStack(spacing: 12) {
+                    ForEach(Array(zip(items.indices, items)), id: \.0) { _, model in
+                        MapSnapshotExample(mapModel: model)
+                    }
+                }
+                .padding()
+            }
         }
     }
 }
